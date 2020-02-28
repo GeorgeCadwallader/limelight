20696201ab1950e811668d3d0cbc6bc195ed5a44
George <georgecadwallader77@gmail.com>
2020-02-28 01:09:08 +0000

    refactor(profile): update user profile view

--- views/profile/user-information.php
+++ views/profile/user-information.php
@@ -3,21 +3,36 @@
 /** @var $userData app\models\UserData */
 
 ?>
+<ul class="list-group mb-3">
+  <li class="list-group-item">
+      <strong>Username:</strong>
+      <?= $userData->user->username; ?>
+  </li>
+  <li class="list-group-item">
+      <strong>Email:</strong>
+      <?= $userData->user->email; ?>
+  </li>
+</ul>
+
 <ul class="list-group">
   <li class="list-group-item">
       <strong>First Name:</strong>
-      <?= $userData->first_name; ?>
+      <?= ($userData->first_name) ? $userData->first_name : ''; ?>
   </li>
   <li class="list-group-item">
       <strong>Last Name:</strong>
-      <?= $userData->last_name; ?>
+      <?= ($userData->last_name) ? $userData->last_name : ''; ?>
+  </li>
+  <li class="list-group-item">
+      <strong>Telephone:</strong>
+      <?= ($userData->telephone) ? $userData->telephone : ''; ?>
   </li>
   <li class="list-group-item">
       <strong>Date of birth:</strong>
-      <?= $userData->date_of_birth; ?>
+      <?= ($userData->date_of_birth) ? $userData->date_of_birth : ''; ?>
   </li>
   <li class="list-group-item">
       <strong>Location:</strong>
-      <?= $userData->county->name.' - '.$userData->county->region->name; ?>
+      <?= ($userData->county) ? $userData->county->name : ''; ?>
   </li>
 </ul>
