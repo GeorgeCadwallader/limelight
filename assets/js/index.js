a3437acd6e27c982cb3f809b8a98b839b81ba0de
George <georgecadwallader77@gmail.com>
2020-03-05 01:35:36 +0000

    feat(review): add star rating widget to project

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,6 +10,8 @@ import '../../vendor/yiisoft/yii2/assets/yii.validation.js';
 import '../../vendor/yiisoft/yii2/assets/yii.gridView.js';
 
 import '../../vendor/bower-asset/bootstrap-datepicker/js/bootstrap-datepicker.js';
+import '../../vendor/kartik-v/bootstrap-star-rating/js/star-rating.js';
+import '../../vendor/kartik-v/bootstrap-star-rating/css/star-rating.css';
 
 import '../../vendor/bower-asset/select2/dist/js/select2.full.js';
 
