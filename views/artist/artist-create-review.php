ec0c60c282fe8d4921a2d713560946269b8fa043
George <georgecadwallader77@gmail.com>
2020-04-03 16:34:14 +0100

    feat(review): add ability to edit review for artist or venue

--- views/artist/artist-create-review.php
+++ views/artist/artist-create-review.php
@@ -26,44 +26,5 @@ use yii\bootstrap4\ActiveForm;
             <span aria-hidden="true">&times;</span>
         </button>
     </div>
-    <?php $form = ActiveForm::begin([
-        'id' => 'create-review',
-    ]); ?>
-        <?= $form->field($newReview, 'content')->textarea(['rows' => 5]); ?>
-        <?= $form->field($newReview, 'overall_rating')->widget(StarRating::class,
-            Yii::$app->params['reviewArtistNew']
-        ); ?>
-        <div class="text-left">
-            <button
-                class="btn btn-primary review-advanced-button my-4"
-                type="button"
-                data-toggle="collapse"
-                data-target="#review-advanced-button"
-                aria-expanded="false"
-                aria-controls="review-advanced-button"
-            >
-                Advanced
-            </button>
-        </div>
-        <div class="collapse" id="review-advanced-button">
-            <?= $form->field($newReview, 'energy')->widget(StarRating::class,
-                Yii::$app->params['reviewArtistNew']
-            ); ?>
-            <?= $form->field($newReview, 'vocals')->widget(StarRating::class,
-                Yii::$app->params['reviewArtistNew']
-            ); ?>
-            <?= $form->field($newReview, 'sound')->widget(StarRating::class,
-                    Yii::$app->params['reviewArtistNew']
-            ); ?>
-            <?= $form->field($newReview, 'stage_presence')->widget(StarRating::class,
-                Yii::$app->params['reviewArtistNew']
-            ); ?>
-            <?= $form->field($newReview, 'song_aesthetic')->widget(StarRating::class,
-                Yii::$app->params['reviewArtistNew']
-            ); ?>
-        </div>
-        <div class="my-4 text-right">
-            <?= Html::submitButton('Save Review', ['class' => 'btn btn-primary']); ?>
-        </div>
-    <?php ActiveForm::end(); ?>
+    <?= $this->render('./partials/artist-review-new', compact('newReview')); ?>
 </div>
---
+++ views/artist/partials/artist-review-new.php
@@ -0,0 +1,52 @@
+<?php
+
+/** @var $newReview app\models\ReviewArtist */
+
+use app\helpers\Html;
+
+use kartik\rating\StarRating;
+
+use yii\bootstrap4\ActiveForm;
+
+?>
+
+<?php $form = ActiveForm::begin([
+        'id' => 'create-review',
+    ]); ?>
+    <?= $form->field($newReview, 'content')->textarea(['rows' => 5]); ?>
+    <?= $form->field($newReview, 'overall_rating')->widget(StarRating::class,
+        Yii::$app->params['reviewArtistNew']
+    ); ?>
+    <div class="text-left">
+        <button
+            class="btn btn-primary review-advanced-button my-4"
+            type="button"
+            data-toggle="collapse"
+            data-target="#review-advanced-button"
+            aria-expanded="false"
+            aria-controls="review-advanced-button"
+        >
+            Advanced
+        </button>
+    </div>
+    <div class="collapse" id="review-advanced-button">
+        <?= $form->field($newReview, 'energy')->widget(StarRating::class,
+            Yii::$app->params['reviewArtistNew']
+        ); ?>
+        <?= $form->field($newReview, 'vocals')->widget(StarRating::class,
+            Yii::$app->params['reviewArtistNew']
+        ); ?>
+        <?= $form->field($newReview, 'sound')->widget(StarRating::class,
+                Yii::$app->params['reviewArtistNew']
+        ); ?>
+        <?= $form->field($newReview, 'stage_presence')->widget(StarRating::class,
+            Yii::$app->params['reviewArtistNew']
+        ); ?>
+        <?= $form->field($newReview, 'song_aesthetic')->widget(StarRating::class,
+            Yii::$app->params['reviewArtistNew']
+        ); ?>
+    </div>
+    <div class="my-4 text-right">
+        <?= Html::submitButton('Save Review', ['class' => 'btn btn-primary']); ?>
+    </div>
+<?php ActiveForm::end(); ?>
