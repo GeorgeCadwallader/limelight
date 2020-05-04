b121df2618ff2bec76158834fd68a5248d556ba9
George <georgecadwallader77@gmail.com>
2020-05-04 16:06:51 +0100

    feat(tests): add testing for new contact functionlality

---
+++ tests/fixtures/ContactFixture.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace app\tests\fixtures;
+
+/**
+ * @category  Project
+ * @package   {{package}}
+ * @author    George Cadwallader <georgecadwallader77@gmail.com>
+ * @copyright 2020
+ */
+class ContactFixture extends \yii\test\ActiveFixture
+{
+
+    /**
+     * The table for the fixture
+     *
+     * @var string
+     */
+    public $tableName = '{{%contact}}';
+
+    /**
+     * The fixtures that this fixture depends on. This must be a list of the
+     * dependent fixture class names
+     *
+     * @var array .
+     */
+    public $depends = [];
+
+}
