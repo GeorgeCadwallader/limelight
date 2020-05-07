32bdb4326d84bf41eff702e038f4dcdd3808077a
George <georgecadwallader77@gmail.com>
2020-05-07 16:56:36 +0100

    feat(profile): change layout of profile view page

--- assets/js/components/review.js
+++ assets/js/components/review.js
@@ -21,10 +21,31 @@ $(document).ready(function() {
             readBtn.css('display', 'none');
         }
     });
+    $('.profile-bio').each(function(i, element) {
+        let height = $(element).css('height').replace('px', '');
+        let readBtn = $(element).siblings('.read-more-profile-btn');
+
+        if (height > '100') {
+            readBtn.css('display', 'none');
+        }
+    });
 });
 
 $('.read-more-btn').click(function() {
     let text = $(this).siblings('.review-text-content');
+    let maxHeight = $(text).css('max-height');
+
+    if (maxHeight === '100px') {
+        $(this).text('Read Less ...');
+        $(text).css('max-height', '100%');
+    } else {
+        $(this).text('Read More ...');
+        $(text).css('max-height', '100px');
+    }
+});
+
+$('.read-more-profile-btn').click(function() {
+    let text = $(this).siblings('.profile-bio');
     let maxHeight = $(text).css('max-height');
 
     if (maxHeight === '100px') {
