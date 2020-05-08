8a6ee811b46b612f1b37f09fc588b8daac1fd538
George <georgecadwallader77@gmail.com>
2020-05-08 17:10:33 +0100

    feat(share): implement sharer.js library into assets

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,14 +10,28 @@ import '../../vendor/yiisoft/yii2/assets/yii.validation.js';
 import '../../vendor/yiisoft/yii2/assets/yii.gridView.js';
 import '../../vendor/bower-asset/yii2-pjax/jquery.pjax.js';
 
+/**
+ * Kartik
+ */
 import '../../vendor/bower-asset/bootstrap-datepicker/js/bootstrap-datepicker.js';
 import '../../vendor/kartik-v/bootstrap-star-rating/js/star-rating.js';
 import '../../vendor/kartik-v/bootstrap-star-rating/css/star-rating.css';
 
+/**
+ * Select2
+ */
 import '../../vendor/bower-asset/select2/dist/js/select2.full.js';
 
+/**
+ * TinyMCE
+ */
 import '../../vendor/tinymce/tinymce/tinymce.js';
 import '../../vendor/tinymce/tinymce/themes/silver/theme.js';
+
+/**
+ * Sharer.js
+ */
+import '../../node_modules/sharer.js/sharer.js';
 
 /**
  * Custom
