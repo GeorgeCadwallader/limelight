86f1f85c9e844a88d9406abc6086d279ed1d0fc0
George <georgecadwallader77@gmail.com>
2020-05-29 17:17:44 +0100

    feat(tests): add more review tone fixture data

--- tests/fixtures/data/review_tone.php
+++ tests/fixtures/data/review_tone.php
@@ -1,3 +1,37 @@
 <?php
 
-return [];
+return [
+    [
+        'review_tone_id' => '1',
+        'fk' => '2',
+        'type' => '1',
+        'score' => '0.75',
+        'tone' => 'joy',
+        'created_at' => '2020-03-06 17:39:10',
+        'updated_at' => '2020-03-06 17:39:11',
+        'created_by' => '7',
+        'updated_by' => '7'
+    ],
+    [
+        'review_tone_id' => '2',
+        'fk' => '2',
+        'type' => '1',
+        'score' => '0.66',
+        'tone' => 'joy',
+        'created_at' => '2020-03-06 17:39:10',
+        'updated_at' => '2020-03-06 17:39:11',
+        'created_by' => '7',
+        'updated_by' => '7'
+    ],
+    [
+        'review_tone_id' => '3',
+        'fk' => '2',
+        'type' => '1',
+        'score' => '0.89',
+        'tone' => 'anger',
+        'created_at' => '2020-03-06 17:39:10',
+        'updated_at' => '2020-03-06 17:39:11',
+        'created_by' => '7',
+        'updated_by' => '7'
+    ],
+];
