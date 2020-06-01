123b7b9372c2a4b404f8dc67608ed49e3e0604b9
George <georgecadwallader77@gmail.com>
2020-06-01 13:35:45 +0100

    feat(advert): change styling of adverts

--- views/advert/partials/advert-single.php
+++ views/advert/partials/advert-single.php
@@ -11,18 +11,21 @@ use yii\helpers\Url;
 if ($model->artist) {
     $url = Url::to(['/artist/view', 'artist_id' => $model->artist->artist_id]);
     $imgUrl = ArtistHelper::imageUrl($model->artist);
+    $name = $model->artist->name;
 } else {
     $url = Url::to(['/venue/view', 'venue_id' => $model->venue->venue_id]);
     $imgUrl = VenueHelper::imageUrl($model->venue);
+    $name = $model->venue->name;
 }
 
 ?>
 
 <a href="<?= $url; ?>">
     <div
-        class="advert-con position-relative text-justify limelight-box-shadow rounded"
+        class="advert-con position-relative limelight-box-shadow rounded"
         style="background-image: url('<?= $imgUrl; ?>')"
     >
-        <?= Html::tag('h4', $model->message, ['class' => 'advert-message']); ?>
+        <?= Html::tag('h4', $model->message, ['class' => 'advert-message-top']); ?>
+        <?= Html::tag('h4', 'View '.$name, ['class' => 'advert-message font-weight-bold']); ?>
     </div>
 </a>
